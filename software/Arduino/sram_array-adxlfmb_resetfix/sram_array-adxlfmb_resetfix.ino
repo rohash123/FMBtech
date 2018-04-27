e0fc0381e9e90a414985b09200a37219e3270263
Venkat Krishnan <myvvkmail@gmail.com>
2018-04-27 00:11:35 -0700

    bug fixes

--- software/Arduino/sram_array-adxlfmb_resetfix/sram_array-adxlfmb_resetfix.ino
+++ software/Arduino/sram_array-adxlfmb_resetfix/sram_array-adxlfmb_resetfix.ino
@@ -5,7 +5,7 @@ int CS = 18;
 int adxlPower = 8;
 
 int incdata = 0;
-int sampleTime = 1000;
+int sampleTime = 50;
 
 int count = 0;
 int adxlResetTimer = 0;
@@ -26,81 +26,87 @@ void setup() {
   SPI.setDataMode(SPI_MODE0);
   Serial.begin(9600);
 
+  setPowerCtl();
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+   if(Serial.available() > 0){
+     incdata = Serial.read();
+      if(incdata == 49){
+        //read - command "1"
+          Serial.println("READ START ==========");
+          for(int i = 0; i < 6970; i++){
+            Serial.print(data[i][0]);
+            Serial.print(", ");
+            Serial.print(data[i][1]);
+            Serial.print(", ");
+            Serial.print(data[i][2]);
+            Serial.print(", ");
+            Serial.print(data[i][3]);
+            Serial.print(", ");
+            Serial.print(data[i][4]);
+            Serial.print(", ");
+            Serial.print(data[i][5]);
+            // Serial.print(", ");
+            // Serial.print(data[i][6]);
+            Serial.println();
+            delay(2);
+           if(Serial.read() == 51){
+              break;
+            }
+          }
+          Serial.println("READ COMPLETE ==========");
+      }else if(incdata ==  50){
+        //erase - command "2"
+        for(int i = 0; i < 6970; i++){
+          for(int j = 0; j < 6; j++){
+            data[i][j] = 0;
+          }
+        }
+       Serial.println("ERASE COMPLETE ==========");
+        count = 0;
+      }else if((incdata == 97) || (incdata == 98) || (incdata == 99) ||
+              (incdata == 100) || (incdata == 101) || (incdata == 102) || 
+              (incdata == 103) || (incdata == 104)){
+       switch(incdata);
+      }
+       switch(incdata){
+     case 97: sampleTime = 5000; // a
+             break;
+     case 98: sampleTime = 2000; // b
+             break;
+     case 99: sampleTime = 1500; // c
+             break;
+     case 100: sampleTime = 1000; // d
+             break;
+     case 101: sampleTime = 500; // e
+             break;
+     case 102: sampleTime = 200; // f
+             break;
+     case 103: sampleTime = 100; // g
+             break;
+     case 104: sampleTime = 50; // h
+             break;
+   }
+   }
+
+  
   
   while(count < 6970){
-
+    setPowerCtl();
+ /*
     if(adxlResetTimer >= 40){
       adxlReset(200);
       setPowerCtl();
       Serial.println("RESET");
     }
-    
-    if(Serial.available() > 0){
-      incdata = Serial.read();
-       if(incdata == 49){
-         //read - command "1"
-           Serial.println("READ START ==========");
-           for(int i = 0; i < 6970; i++){
-             Serial.print(data[i][0]);
-             Serial.print(", ");
-             Serial.print(data[i][1]);
-             Serial.print(", ");
-             Serial.print(data[i][2]);
-             Serial.print(", ");
-             Serial.print(data[i][3]);
-             Serial.print(", ");
-             Serial.print(data[i][4]);
-             Serial.print(", ");
-             Serial.print(data[i][5]);
-             // Serial.print(", ");
-             // Serial.print(data[i][6]);
-             Serial.println();
-             delay(2);
-            if(Serial.read() == 51){
-               break;
-             }
-           }
-           Serial.println("READ COMPLETE ==========");
-       }else if(incdata ==  50){
-         //erase - command "2"
-         for(int i = 0; i < 6970; i++){
-           for(int j = 0; j < 6; j++){
-             data[i][j] = 0;
-           }
-         }
-        Serial.println("ERASE COMPLETE ==========");
-         count = 0;
-       }else if((incdata == 97) || (incdata == 98) || (incdata == 99) ||
-               (incdata == 100) || (incdata == 101) || (incdata == 102) || 
-               (incdata == 103) || (incdata == 104)){
-        switch(incdata);
-       }
-    }
 
-   switch(incdata){
-      case 97: sampleTime = 5000; // a
-              break;
-      case 98: sampleTime = 2000; // b
-              break;
-      case 99: sampleTime = 1500; // c
-              break;
-      case 100: sampleTime = 1000; // d
-              break;
-      case 101: sampleTime = 500; // e
-              break;
-      case 102: sampleTime = 200; // f
-              break;
-      case 103: sampleTime = 100; // g
-              break;
-      case 104: sampleTime = 50; // h
-              break;
-    }
-
-    // possible error due to too high freq input ->
+ */
+ 
   
    /*   
     SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
@@ -113,6 +119,66 @@ void loop() {
     SPI.endTransaction();
 
     */
+
+    if(Serial.available() > 0){
+     incdata = Serial.read();
+      if(incdata == 49){
+        //read - command "1"
+          Serial.println("READ START ==========");
+          for(int i = 0; i < 6970; i++){
+            Serial.print(data[i][0]);
+            Serial.print(", ");
+            Serial.print(data[i][1]);
+            Serial.print(", ");
+            Serial.print(data[i][2]);
+            Serial.print(", ");
+            Serial.print(data[i][3]);
+            Serial.print(", ");
+            Serial.print(data[i][4]);
+            Serial.print(", ");
+            Serial.print(data[i][5]);
+            // Serial.print(", ");
+            // Serial.print(data[i][6]);
+            Serial.println();
+            delay(2);
+           if(Serial.read() == 51){
+              break;
+            }
+          }
+          Serial.println("READ COMPLETE ==========");
+      }else if(incdata ==  50){
+        //erase - command "2"
+        for(int i = 0; i < 6970; i++){
+          for(int j = 0; j < 6; j++){
+            data[i][j] = 0;
+          }
+        }
+       Serial.println("ERASE COMPLETE ==========");
+        count = 0;
+      }else if((incdata == 97) || (incdata == 98) || (incdata == 99) ||
+              (incdata == 100) || (incdata == 101) || (incdata == 102) || 
+              (incdata == 103) || (incdata == 104)){
+       switch(incdata);
+      }
+       switch(incdata){
+     case 97: sampleTime = 5000; // a
+             break;
+     case 98: sampleTime = 2000; // b
+             break;
+     case 99: sampleTime = 1500; // c
+             break;
+     case 100: sampleTime = 1000; // d
+             break;
+     case 101: sampleTime = 500; // e
+             break;
+     case 102: sampleTime = 200; // f
+             break;
+     case 103: sampleTime = 100; // g
+             break;
+     case 104: sampleTime = 50; // h
+             break;
+   }
+   }
 
     // <-
 /*    
@@ -152,6 +218,7 @@ void loop() {
     count++;
     adxlResetTimer++;
    }
+
 }
 
 void adxlReset(int delayTime){
